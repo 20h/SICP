28a47a1685bb9f8262bd0f516de91dc824a4e1dc
20h <twentyh@gmail.com>
2014-03-05 23:50:12 +0900

    ex1.3
    return sum of the squares of the two larger numbers

---
+++ ex1_3.scm
@@ -0,0 +1,16 @@
+(define (Square a)
+	(* a a)
+)
+
+(define (SquareThreeSum a b c)
+	(+ (* a a) (* b b) (* c c))
+)
+
+(define (SquareTwoSumOfThree a b c)
+	(- (SquareThreeSum a b c)
+		(cond	((and (not (> a b)) (not (> a c))) (Square a))
+				((and (not (< b a)) (not (< b c))) (Square b))
+				(else (Square c))
+		)
+	)
+)
