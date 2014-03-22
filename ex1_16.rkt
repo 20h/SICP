edfe8d42d996fefa619f6e027dbbb9fe60d02688
20h <twentyh@gmail.com>
2014-03-22 11:20:10 +0900

    1.16

---
+++ ex1_16.rkt
@@ -0,0 +1,30 @@
+#lang racket
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+
+
+;;;; iterative process
+(define (expt b n)
+  (fast-expt-iter b n 1))
+
+(define (fast-expt-iter b n a)
+  (cond ((< n 1)   a)
+        ((even? n) (fast-expt-iter b (/ n 2) (* a (expt b (/ n 2)))))
+        (else      (fast-expt-iter b (- n 1) (* a b)))
+  )
+)
+
+
+;;;; recursive process
+(define (square x) (* x x))
+
+(define (fast-expt-recu b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt-recu b (/ n 2))))
+        (else (* b (fast-expt-recu b (- n 1))))))
+
+
+(expt 2 1000000)
+;(fast-expt-recu 2 100000)
