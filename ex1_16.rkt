76c1959053607fa835f037c44e8c866f21c01759
20h <twentyh@gmail.com>
2014-03-22 11:51:50 +0900

    1.16 효율적으로 수정

--- ex1_16.rkt
+++ ex1_16.rkt
@@ -11,7 +11,7 @@
 
 (define (fast-expt-iter b n a)
   (cond ((< n 1)   a)
-        ((even? n) (fast-expt-iter b (/ n 2) (* a (expt b (/ n 2)))))
+        ((even? n) (fast-expt-iter (* b b) (/ n 2) a))
         (else      (fast-expt-iter b (- n 1) (* a b)))
   )
 )
@@ -26,5 +26,5 @@
         (else (* b (fast-expt-recu b (- n 1))))))
 
 
-(expt 2 1000000)
+(expt 2 10)
 ;(fast-expt-recu 2 100000)
